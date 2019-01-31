2554b6a9d75ec370da99158a89a6ab621a02e651
Vaibhav Agarwal <42164841+agvaibhav@users.noreply.github.com>
2019-01-31 22:14:16 +0530

    Create min_swaps_brackets.py

---
+++ min_swaps_brackets.py
@@ -0,0 +1,31 @@
+def swapCount(s):
+    pos=[]
+    for i in range(len(s)):
+        if s[i]=='[':
+            pos.append(i)
+
+    count = 0
+    p = 0
+    ans = 0
+
+    for i in range(len(s)):
+        if s[i]=='[':
+            count+=1
+            p+=1
+
+        elif s[i]==']':
+            count-=1
+
+        if count < 0:
+            ans += pos[p] - i
+            s[i],s[pos[p]] = s[pos[p]],s[i]
+            p+=1
+
+            count = 1
+            
+    return ans
+    
+s='[[][]]'
+s=list(s)
+res=swapCount(s)
+print(res)
